1af051904f287f023fc61e5156d65f541e373e90
M Jesús Otero <mjesuotero@airbears2-10-142-145-68.airbears2.1918.berkeley.edu>
2018-04-30 15:33:01 -0700

    randomTargetNumber between 19 and 120

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
     // Here we establish the 'randomTargerNumber' between 19 - 120.
-    var randomTargerNumber = Math.round(Math.random() * 120) + 1;
-    // alert(randomTargerNumber);
+    var randomTargerNumber = Math.floor(Math.random() * 102) + 19;
+    alert(randomTargerNumber);
     
+    
+
+    alert(randomTargerNumber);
     // put targerNumber insie de span id = number-to-guess
     $('#number-to-guess').text(randomTargerNumber);
 
@@ -12,23 +15,15 @@ $(document).ready(function() {
     var wins = 0;
     // Placeholder for losses
     var losses = 0;
-    
-
     // Each crystal should have a random hidden value between 1 - 12.
-    var c1 = Math.round(Math.random() * 12) + 1;
-    var c2 = Math.round(Math.random() * 11) + 1;
-    var c3 = Math.round(Math.random() * 10) + 1;
-    var c4 = Math.round(Math.random() * 9) + 1;
-    var randomNumberOptions = [c1, c2, c3, c4];
-    console.log(c1);
-
+    var randomNumberOptions = Math.floor(Math.random() * 12) + 1 ;
 
 
     // FUNCTION
     // EVENT click C1
     $('#c1').on('click', function() {
         // When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
-        counter += randomNumberOptions[0];
+        counter += randomNumberOptions;
         console.log(c1);
         $('#counter').text(counter);
             // Here we create some logic to 'check' if click counter matches with randomTargerNumber.
@@ -48,7 +43,7 @@ $(document).ready(function() {
     // EVENT click C2
     $('#c2').on('click', function() {
         // When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
-        counter += randomNumberOptions[1];
+        counter += randomNumberOptions;
         $('#counter').text(counter);
             // Here we create some logic to 'check' if click counter matches with randomTargerNumber.
             if (counter === randomTargerNumber) {
@@ -67,7 +62,7 @@ $(document).ready(function() {
     // EVENT click C3
     $('#c3').on('click', function() {
         // When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
-        counter += randomNumberOptions[2];
+        counter += randomNumberOptions;
         $('#counter').text(counter);
             // Here we create some logic to 'check' if click counter matches with randomTargerNumber.
             if (counter === randomTargerNumber) {
@@ -86,7 +81,7 @@ $(document).ready(function() {
     // EVENT click C4
     $('#c4').on('click', function() {
         // When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
-        counter += randomNumberOptions[3];
+        counter += randomNumberOptions;
         $('#counter').text(counter);
         // Here we create some logic to 'check' if click counter matches with randomTargerNumber.
         if (counter === randomTargerNumber) {
@@ -108,14 +103,9 @@ function reset(){
     // randomTargerNumber = Math.floor(Math.random() * 50);
     var minTargerNumber = 30; // minimum
     var maxTargerNumber = 50; // maximum
-    var randomTargerNumber = Math.floor(Math.random() * (maxTargerNumber + 1 ) + minTargerNumber);
+    var randomTargerNumber = Math.floor(Math.random() * 102) + 19;
     $('#number-to-guess').text(randomTargerNumber);
-    c1 = Math.round(Math.random() * 8) + 1;
-    c2 = Math.round(Math.random() * 7) + 1;
-    c3 = Math.round(Math.random() * 6) + 1;
-    c4 = Math.round(Math.random() * 5) + 1;
+    var randomNumberOptions = Math.floor(Math.random() * 12) + 1;
     
 }
-
-
 });
